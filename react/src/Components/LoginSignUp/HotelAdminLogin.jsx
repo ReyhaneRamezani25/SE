769ffd7f784c91d92d3b5942d5a558d84fa70df5
ramezanir25121381 <reyhaneramezani11@gmail.com>
2024-03-08 14:46:42 +0330

    hoteladmin added

---
+++ react/src/Components/LoginSignUp/HotelAdminLogin.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './LoginSignUp.css';
+
+const HotelAdminLogin = () => {
+ 
+  return (
+    <div className='container'>
+      <div className='header'>
+        <div className='text'>Login</div>
+        <div className='underLine'></div>
+      </div>
+      <form >
+        <div className='inputs'>
+          <div className='input'>
+            <img src={require('./icon_user.png')} alt='' />
+            <input
+              type='text'
+              placeholder='UserName'
+            />
+          </div>
+
+          <div className='input'>
+            <img src={require('./icon_pass.png')} alt='' />
+            <input
+              type='password'
+              placeholder='Password'
+            />
+          </div>
+        </div>
+
+        <div className='submit-container'>
+          <div
+            className='submit'
+          >
+            Login
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default HotelAdminLogin;
