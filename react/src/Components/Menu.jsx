dbce329d92bdaa06e8b89f2b5d8477e7dc881a90
Shahrabi.Soheil <shahrabi.soheil@yahoo.com>
2024-05-24 23:15:59 +0330

    Fix every hotels page.

--- react/src/Components/Menu.jsx
+++ react/src/Components/Menu.jsx
@@ -8,6 +8,9 @@ import { UserContext } from '../UserContext';
 
 const Menu = () => {
   const [imageSrcs, setImageSrcs] = useState([]);
+  const [hotelIds, setHotelIds] = useState([]);
+  const [hotelNames, setHotelNames] = useState([]);
+  
   const {term, date, date_end} = useContext(UserContext);
   console.log(term, date, date_end);
   const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
@@ -50,8 +53,12 @@ const Menu = () => {
     })
     
     .then(data => {  
-      console.log(JSON.parse(data).image_urls)
+      console.log(JSON.parse(data))
       const imageUrls = JSON.parse(data).image_urls;
+      const hotel_names = JSON.parse(data).names;
+      const hotel_ids = JSON.parse(data).id;
+      setHotelIds(hotel_ids);
+      setHotelNames(hotel_names);
       if (imageSrcs.length === 0){
         for (const url of imageUrls) {
           fetchImage(url);
@@ -67,25 +74,31 @@ const Menu = () => {
 
   },[]);
 
+  useEffect(() => {
+    console.log('Hotel IDs after state update:', hotelIds);
+  }, [hotelIds]);
+
   const handleClick = (index) => {
-    // Navigate to Hotel component with index as parameter
     window.location.href = `/hotel/${index}`;
   };
 
-  return (
-    <div className='menu-container'>
-        <div className='header'>
-            {/* Render the images */}
-            <div className="menu-columns">
-                {imageSrcs.map((imageUrl, index) => (
-                    <div key={index} className="menu-item" onClick={() => handleClick(index)}>
-                        <img src={imageUrl} alt={`Image ${index}`} />
-                    </div>
-                ))}
+return (
+  <div className='menu-container'>
+    <div className='header'>
+      <div className="menu-columns">
+        {imageSrcs.map((imageUrl, index) => (
+          <div key={index} className="image-columns">
+            <div className="menu-item" onClick={() => handleClick(hotelIds[index])}>
+              <img src={imageUrl} alt={`Image ${index}`} />
             </div>
-        </div>
+            <div className="hotel-id">هتل: {hotelNames[index]}</div> {/* Display hotel ID on the right */}
+          </div>
+        ))}
+      </div>
     </div>
+  </div>
 );
+
 };
 
 export default Menu;
