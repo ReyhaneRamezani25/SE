ccafdc9da6958ade129724f38445cb021d9bc39f
Zahra Tayebi <s.z.tayebi.j@gmail.com>
2024-06-13 13:17:28 +0330

    setting edited.

--- django/pythonProject/BookHotel/BookHotel/settings.py
+++ django/pythonProject/BookHotel/BookHotel/settings.py
@@ -18,9 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-leau^^aoyq)bhtt=h@#g!lclr^)ma&+adpsw!(i65l$_k7ux5n'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
