e48f793045405e3a4d828dd87379f7e48ca27523
ramezanir25121381 <reyhaneramezani11@gmail.com>
2024-07-03 20:28:04 +0330

    HotelRefactor updated

--- react/src/Components/HotelStrict/HotelRefactor.jsx
+++ react/src/Components/HotelStrict/HotelRefactor.jsx
@@ -367,7 +367,7 @@ const HotelRefactor = () => {
         <div className="reserve-popup">
           <div className="reserve-popup-content">
             <div className="custom-image-room" onClick={() => handleImageClick('newRoomFileInput')}>
-              <img alt="room Image" src={newRoomImage} className="custom-image-room" />
+              <img alt="rooms" src={newRoomImage} className="custom-image-room" />
               <input
                 id="newRoomFileInput"
                 type="file"
